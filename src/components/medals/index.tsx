1acf5e74a942435f4fb3ddbfdec3991d52ac3ed1
Mohammad Baragbah <duniandewon@gmail.com>
2023-02-13 17:21:18 +0700

    Refactor main content styles

---
+++ src/components/medals/index.tsx
@@ -0,0 +1,127 @@
+import { useMemo } from "react";
+import { Medal } from "@/assets/icons";
+
+const medals = [
+  {
+    country: "Indonesia",
+    gold: 1,
+    silver: 2,
+    bronze: 3,
+    total: 300,
+  },
+  {
+    country: "Cina",
+    gold: 1,
+    silver: 2,
+    bronze: 3,
+    total: 300,
+  },
+  {
+    country: "Filipn",
+    gold: 1,
+    silver: 2,
+    bronze: 3,
+    total: 300,
+  },
+  {
+    country: "Malaysia",
+    gold: 1,
+    silver: 2,
+    bronze: 3,
+    total: 300,
+  },
+  {
+    country: "Tailand",
+    gold: 1,
+    silver: 2,
+    bronze: 3,
+    total: 300,
+  },
+  {
+    country: "India",
+    gold: 1,
+    silver: 2,
+    bronze: 3,
+    total: 300,
+  },
+  {
+    country: "Pakistan",
+    gold: 1,
+    silver: 2,
+    bronze: 3,
+    total: 300,
+  },
+  {
+    country: "Iran",
+    gold: 1,
+    silver: 2,
+    bronze: 3,
+    total: 300,
+  },
+  {
+    country: "Iraq",
+    gold: 1,
+    silver: 2,
+    bronze: 3,
+    total: 300,
+  },
+  {
+    country: "Qatar",
+    gold: 1,
+    silver: 2,
+    bronze: 3,
+    total: 300,
+  },
+  {
+    country: "Bosnia",
+    gold: 1,
+    silver: 2,
+    bronze: 3,
+    total: 300,
+  },
+  {
+    country: "Kenya",
+    gold: 1,
+    silver: 2,
+    bronze: 3,
+    total: 300,
+  },
+  {
+    country: "Tanzania",
+    gold: 1,
+    silver: 2,
+    bronze: 3,
+    total: 300,
+  },
+];
+
+const Medals = () => {
+  return (
+    <section className="medals">
+      <div className="medals__row">
+        <div className="medals__number">
+          <Medal />
+        </div>
+        <div className="medals__country">Negara</div>
+        <div className="medals__medal medals__medal--gold"></div>
+        <div className="medals__medal medals__medal--silver"></div>
+        <div className="medals__medal medals__medal--bronz"></div>
+      </div>
+      {useMemo(
+        () =>
+          medals.map((medal) => (
+            <div className="medals__row" key={medal.country}>
+              <div className="medals__number">1</div>
+              <div className="medals__country">{medal.country}</div>
+              <div className="medals__medal">{medal.gold}</div>
+              <div className="medals__medal">{medal.silver}</div>
+              <div className="medals__medal">{medal.bronze}</div>
+            </div>
+          )),
+        []
+      )}
+    </section>
+  );
+};
+
+export default Medals;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,108 +1,13 @@
-import { GetStaticProps, InferGetStaticPropsType } from "next";
 import Image from "next/image";
 import { useMemo } from "react";
 
-import { Medal } from "@/assets/icons";
 import Wrapper from "@/components/Wrapper";
 
 import { usePostsPreview } from "@/hooks";
 
 import { IPost } from "@/interfaces";
 import Link from "next/link";
-
-const medals = [
-  {
-    country: "Indonesia",
-    gold: 1,
-    silver: 2,
-    bronze: 3,
-    total: 300,
-  },
-  {
-    country: "Cina",
-    gold: 1,
-    silver: 2,
-    bronze: 3,
-    total: 300,
-  },
-  {
-    country: "Filipn",
-    gold: 1,
-    silver: 2,
-    bronze: 3,
-    total: 300,
-  },
-  {
-    country: "Malaysia",
-    gold: 1,
-    silver: 2,
-    bronze: 3,
-    total: 300,
-  },
-  {
-    country: "Tailand",
-    gold: 1,
-    silver: 2,
-    bronze: 3,
-    total: 300,
-  },
-  {
-    country: "India",
-    gold: 1,
-    silver: 2,
-    bronze: 3,
-    total: 300,
-  },
-  {
-    country: "Pakistan",
-    gold: 1,
-    silver: 2,
-    bronze: 3,
-    total: 300,
-  },
-  {
-    country: "Iran",
-    gold: 1,
-    silver: 2,
-    bronze: 3,
-    total: 300,
-  },
-  {
-    country: "Iraq",
-    gold: 1,
-    silver: 2,
-    bronze: 3,
-    total: 300,
-  },
-  {
-    country: "Qatar",
-    gold: 1,
-    silver: 2,
-    bronze: 3,
-    total: 300,
-  },
-  {
-    country: "Bosnia",
-    gold: 1,
-    silver: 2,
-    bronze: 3,
-    total: 300,
-  },
-  {
-    country: "Kenya",
-    gold: 1,
-    silver: 2,
-    bronze: 3,
-    total: 300,
-  },
-  {
-    country: "Tanzania",
-    gold: 1,
-    silver: 2,
-    bronze: 3,
-    total: 300,
-  },
-];
+import Medals from "@/components/medals";
 
 export default function Home() {
   const { data } = usePostsPreview();
@@ -120,6 +25,7 @@ export default function Home() {
         </div>
       </header>
       <section className="main-content">
+        <Medals />
         <section id="news-list" className="news-list">
           {useMemo(
             () =>
@@ -155,31 +61,6 @@ export default function Home() {
                 </div>
               )),
             [data]
-          )}
-        </section>
-
-        <section className="medals">
-          <div className="medals__row">
-            <div className="medals__number">
-              <Medal />
-            </div>
-            <div className="medals__country">Negara</div>
-            <div className="medals__medal medals__medal--gold"></div>
-            <div className="medals__medal medals__medal--silver"></div>
-            <div className="medals__medal medals__medal--bronz"></div>
-          </div>
-          {useMemo(
-            () =>
-              medals.map((medal) => (
-                <div className="medals__row" key={medal.country}>
-                  <div className="medals__number">1</div>
-                  <div className="medals__country">{medal.country}</div>
-                  <div className="medals__medal">{medal.gold}</div>
-                  <div className="medals__medal">{medal.silver}</div>
-                  <div className="medals__medal">{medal.bronze}</div>
-                </div>
-              )),
-            []
           )}
         </section>
       </section>
