9cf2c7627aba5bc55fda2fe285c38f7895e42bee
Mohammad Baragbah <duniandewon@gmail.com>
2023-02-14 10:30:12 +0700

    Create post page

--- src/components/PostPreview/index.tsx
+++ src/components/PostPreview/index.tsx
@@ -7,11 +7,17 @@ const PostPreview = ({ post }: { post: IPost }) => {
   return (
     <div className="news">
       {post.featuredImage && (
-        <div className="news__thumbnail">
-          <Image src={post.featuredImage.node.sourceUrl} alt="thumbnail" />
+        <div className="thumbnail">
+          <Image src={post.featuredImage.node.sourceUrl} alt="thumbnail" fill />
         </div>
       )}
       <div className="news__main-content">
+        <Link href={`/posts/${post.slug}`}>
+          <h2
+            className="news__title"
+            dangerouslySetInnerHTML={{ __html: post.title }}
+          />
+        </Link>
         <div className="news__date">
           <div className="news__icon">
             {/* <img src="/icons/voli.svg" alt="voli" /> */}
@@ -19,12 +25,6 @@ const PostPreview = ({ post }: { post: IPost }) => {
           <div className="news__separator"></div>
           <div className="news__date">{post.date}</div>
         </div>
-        <Link href={`/posts/${post.slug}`}>
-          <h2
-            className="news__title"
-            dangerouslySetInnerHTML={{ __html: post.title }}
-          />
-        </Link>
         <p
           className="news__description"
           dangerouslySetInnerHTML={{ __html: post.excerpt }}
--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -1,7 +1,13 @@
 import { useRouter } from "next/router";
 
 import Wrapper from "@/components/Wrapper";
+import PostPreview from "@/components/PostPreview";
+import Medals from "@/components/medals";
+
 import { usePost } from "@/hooks";
+
+import { IPost } from "@/interfaces";
+import Image from "next/image";
 
 const Post = () => {
   const { query } = useRouter();
@@ -9,15 +15,49 @@ const Post = () => {
   const { slug } = query;
 
   const {
-    data: { post, posts: morePosts },
+    data: { post, posts },
   } = usePost(slug);
 
+  const renderArticle = () => (
+    <article className="article">
+      <h1 dangerouslySetInnerHTML={{ __html: post?.title }}></h1>
+      <div className="news__date">
+        <div className="news__icon">
+          {/* <img src="/icons/voli.svg" alt="voli" /> */}
+        </div>
+        <div className="news__separator"></div>
+        <div className="news__date">{post?.date}</div>
+      </div>
+      <div className="image">
+        <Image
+          src={post?.featuredImage?.node?.sourceUrl}
+          alt="thumbnail"
+         fill
+        />
+      </div>
+      <div dangerouslySetInnerHTML={{ __html: post?.content }} />
+      <div dangerouslySetInnerHTML={{ __html: post?.content }} />
+    </article>
+  );
+
+  const renderMorePosts = () => (
+    <div className="more-posts">
+      {posts?.map(
+        (post: IPost) =>
+          post?.slug !== slug && <PostPreview post={post} key={post.id} />
+      )}
+    </div>
+  );
+
   return (
-    <Wrapper title="">
-      <article>
-        <h1 dangerouslySetInnerHTML={{ __html: post?.title }}></h1>
-        <div dangerouslySetInnerHTML={{ __html: post?.content }} />
-      </article>
+    <Wrapper title={post?.title}>
+      <div className="post">
+        {renderArticle()}
+        <aside className="post__aside">
+          {renderMorePosts()}
+          <Medals />
+        </aside>
+      </div>
     </Wrapper>
   );
 };
